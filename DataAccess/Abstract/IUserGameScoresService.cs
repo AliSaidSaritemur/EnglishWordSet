707b911101b001b434854e66bf0db4ce032e402f
Said Saritemur <AliSaidSaritemur@gmail.com>
2023-05-27 16:15:17 +0300

    Add Game Score's Services

---
+++ DataAccess/Abstract/IUserGameScoresService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    internal interface IUserGameScoresService
+    {
+         void AddNewLastLetterGameScore(string username, int score);
+         void UpdateLastLetterGameBestScore(string username, int score);
+        void CleanUsersLastLetterGameScores(string username);
+        void AddNewScoreLastLetterGameIfBiggerThanBestScoreUpdateBestScore(string username, int score);
+        int GetLastLetterGameBestScore(string username);
+    }
+}
---
+++ DataAccess/Concrete/UserGameScoresImpl.cs
@@ -0,0 +1,51 @@
+﻿using DataAccess.Abstract;
+using DataAccess.util;
+using Entities.Concrete;
+using LogAccess.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    internal class UserGameScoresImpl : IUserGameScoresService
+    {
+        private WordContext context = MyDBTransactions.GetContext();
+        public void AddNewLastLetterGameScore(string username, int score)
+        {
+            UserGameScore userGameScores = context._UserGameScores.FirstOrDefault(I => I.UserName == username);
+            userGameScores.LastLetterGameScores = score + "\n" + userGameScores.LastLetterGameScores;
+            context.SaveChanges();
+        }
+
+        public void AddNewScoreLastLetterGameIfBiggerThanBestScoreUpdateBestScore(string username, int score)
+        {
+            AddNewLastLetterGameScore( username,  score);
+            int currentBestScore = GetLastLetterGameBestScore(username);
+            if (score > currentBestScore)
+                UpdateLastLetterGameBestScore(username, score); 
+        }
+
+        public void CleanUsersLastLetterGameScores(string username)
+        {
+            UserGameScore userGameScores = context._UserGameScores.FirstOrDefault(I => I.UserName == username);
+            userGameScores.LastLetterGameScores = " ";
+            context.SaveChanges();
+        }
+
+        public int GetLastLetterGameBestScore(string username)
+        {
+            UserGameScore userGameScores = context._UserGameScores.FirstOrDefault(I => I.UserName == username);
+           return userGameScores.LastLetterGameBestScore;
+        }
+
+        public void UpdateLastLetterGameBestScore(string username, int score)
+        {
+            UserGameScore userGameScores = context._UserGameScores.FirstOrDefault(I => I.UserName == username);
+            userGameScores.LastLetterGameBestScore = score;
+            context.SaveChanges();
+        }
+    }
+}
